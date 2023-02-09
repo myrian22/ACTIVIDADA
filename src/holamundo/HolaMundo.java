0e1f815e44ce97ddcc5136ca67b9f93531f95639
ERICK <ERICK@DESKTOP-DL3BQBQ>
2023-02-09 10:10:56 +0100

    2

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -1,7 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package holamundo;
 
 /**
@@ -15,6 +13,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola soy jose ");
     }
     
 }
